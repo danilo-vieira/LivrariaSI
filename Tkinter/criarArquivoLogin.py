dad911ce127bb953d0ea1f5a9bec4000f53b10aa
Wellington R <tinho1068@hotmail.com>
2019-06-14 15:36:39 -0300

    Commit na login.py
    commit para alteraçao do caminho das imagens do programa . Colocando meu nome

--- Tkinter/criarArquivoLogin.py
+++ Tkinter/criarArquivoLogin.py
@@ -1,4 +1,4 @@
 import shelve
-db = shelve.open('C:\\Users\\danil\\Desktop\\Tkinter\\arquivosdb\\login\\login.db')
+db = shelve.open('C:\\Users\\wellington\\Desktop\\Tkinter\\arquivosdb\\login\\login.db')
 db['User'] = []
 db['Pass'] = []
